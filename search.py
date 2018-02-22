0e7810c51ca5dd1f1cd38d7b67c3aad257742a22
Cheng Liangyu <clyfish@gmail.com>
2018-02-22 23:04:52 +0800

    use bisect

--- search.py
+++ search.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python
-import sys, os, mmap, struct, getpass, hashlib
+import sys, os, mmap, struct, bisect, getpass, hashlib
 
-f = open(os.path.dirname(sys.argv[0]) + '/pwned-passwords-2.0.bin')
-mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-size = len(mm) / 24
+class SortedPasswd:
+    def __init__(self):
+        f = open(os.path.dirname(sys.argv[0]) + '/pwned-passwords-2.0.bin')
+        self.mm = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+    def __len__(self):
+        return len(self.mm) / 24
+    def __getitem__(self, i):
+        return self.mm[i * 24 : i * 24 + 20]
+    def count(self, i):
+        return struct.unpack('<i', self.mm[i * 24 + 20 : i * 24 + 24])[0]
 
-def item(i):
-    return mm[i * 24 : i * 24 + 20]
-
-def count(i):
-    return struct.unpack('<i', mm[i * 24 + 20 : i * 24 + 24])[0]
+sortedPasswd = SortedPasswd()
 
 def search(x):
-    left = 0
-    right = size
+    index = bisect.bisect_left(sortedPasswd, x)
 
-    while left < right:
-        mid = (left + right) // 2
-
-        if item(mid) < x:
-            left = mid + 1
-        else:
-            right = mid
-
-    if left < size and item(left) == x:
-        return count(left)
+    if index < len(sortedPasswd) and sortedPasswd[index] == x:
+        return sortedPasswd.count(index)
     else:
         return 0
 
